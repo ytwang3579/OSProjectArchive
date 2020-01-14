e8e82149ad4c66f99e7a1e7964068d4b39cb5043
ytwang <ytwang3579@gapp.nthu.edu.tw>
2020-01-14 23:09:49 +0800

    ppc5 : seemly ok

--- PPC5/preemptive.c
+++ PPC5/preemptive.c
@@ -9,7 +9,9 @@ __data __at (0x37) char i;
 __data __at (0x38) char tmp;
 __data __at (0x39) char tmp2;
 __data __at (0x3A) char checkAlive[MAXTHREADS];
-__data __at (0x3E) unsigned char now;
+__data __at (0x3E) unsigned char cnt;
+__data __at (0x3F) unsigned char now;
+__data __at (0x2C) unsigned char delayid[4];
 
 #define SAVESTATE \
 	{ \
@@ -38,12 +40,14 @@ __data __at (0x3E) unsigned char now;
 extern void main(void);
 
 void Bootstrap(void) {
+	cnt = 0;
 	now = 0;
 	TMOD = 0;
 	IE = 0x82;
 	TR0 = 1;
 	bitmap = 0b0000;
 	checkAlive[0] = 0b0001; checkAlive[1] = 0b0010; checkAlive[2] = 0b0100; checkAlive[3] = 0b1000;
+	delayid[0] = delayid[1] = delayid[2] = delayid[3] = 0;
 	curThread = ThreadCreate(main);
 	RESTORESTATE;
 }
@@ -95,6 +99,18 @@ void ThreadYield(void) {
 
 void ThreadExit(void) {
 	__critical{
+		__asm 
+		mov A, #0
+		push A 
+		push A 
+		push A 
+		push A
+		push A
+		push A
+		push A
+		push A
+		__endasm; 
+		sp[curThread] = 0;
 		bitmap &= ~(1<<curThread);
 		do {
 			curThread = (curThread == MAXTHREADS-1) ? 0 : curThread+1;
@@ -105,13 +121,15 @@ void ThreadExit(void) {
 }
 
 void myTimer0Handler(void) {
+	EA=0;
+		
 		SAVESTATE;
 		__asm
 			mov ACC, r1
 			mov B, r2
 			mov DPL, r3
 			mov DPH, r4
-			mov PSW, r5
+			mov _i, r5
 			mov _tmp, r6
 			mov _tmp2, r7
 		__endasm;
@@ -119,25 +137,32 @@ void myTimer0Handler(void) {
 			curThread = (curThread == MAXTHREADS-1) ? 0 : curThread+1;
 			if(bitmap & checkAlive[curThread]) break;
 		} while (1);
+		cnt++;
+		if(cnt==4) {
+			cnt = 0;
+			now++;
+		}
 		__asm
 			mov r1, ACC
 			mov r2, B
 			mov r3, DPL
 			mov r4, DPH
-			mov r5, PSW
+			mov r5, _i
 			mov r6, _tmp
 			mov r7, _tmp2
 		__endasm;
 		RESTORESTATE;
-		now++;
+	EA=1;
 		__asm
 			reti
 		__endasm;
+	
 }
 
 void delay(unsigned char n) {
-	char a = (now + n) % 256;
-	while(now != a);
+	delayid[curThread] = now + n;
+	while(now != delayid[curThread]) EA=1;
+	delayid[curThread] = 0;
 }
 
 
--- PPC5/testparking.c
+++ PPC5/testparking.c
@@ -9,51 +9,34 @@ __data __at (0x24) Semaphore spotsSync;
 __data __at (0x25) char spots[2];
 __data __at (0x26) char head;
 __data __at (0x27) char tail;
-__data __at (0x28) char i;
 __data __at (0x29) char buf[7];
-__data __at (0x3E) unsigned char now;
+__data __at (0x35) char bitmap;
+__data __at (0x3F) unsigned char now;
 __code __at (0x70) char op[15] = {'C','A','R',':','@','[',',',']','\n'};
 
 void Car1(void){
 	SemaphoreWait(sem);
-
+	
 	__critical{
-		SemaphoreWait(mutex);
+		SemaphoreWait(spotsSync);
 		if(spots[0]=='X') {
 			spots[0] = '1';
 		} else {
 			spots[1] = '1';
 		}
-		buf[0] = '1';
-		buf[1] = ' ';
-		buf[2] = 'I';
-		buf[3] = 'N';
-		buf[4] = spots[0];
-		buf[5] = spots[1];
-		buf[6] = now;
-		SemaphoreSignal(mutex);
+		SemaphoreSignal(spotsSync);
 	}
 
 	delay(7);
-
+	
 	__critical{
-		SemaphoreWait(mutex);
+		SemaphoreWait(spotsSync);
 		if(spots[0]=='1') {
 			spots[0] = 'X';
 		} else {
 			spots[1] = 'X';
 		}
-		op[3] = '1';
-		op[5] = 'O';
-		op[6] = 'U';
-		op[7] = 'T';
-		op[10] = spots[0];
-		op[12] = spots[1];
-		for(i=0; i<15; i++) {
-			buf[head] = op[i];
-			head = (head==15) ? 0 : head + 1;
-		}
-		SemaphoreSignal(mutex);
+		SemaphoreSignal(spotsSync);
 	}
 
 	SemaphoreSignal(sem);
@@ -64,47 +47,27 @@ void Car1(void){
 
 void Car2(void){
 	SemaphoreWait(sem);
-
+	
 	__critical{
-		SemaphoreWait(mutex);
+		SemaphoreWait(spotsSync);
 		if(spots[0]=='X') {
 			spots[0] = '2';
 		} else {
 			spots[1] = '2';
 		}
-		op[3] = '2';
-		op[5] = ' ';
-		op[6] = 'I';
-		op[7] = 'N';
-		op[10] = spots[0];
-		op[12] = spots[1];
-		for(i=0; i<15; i++) {
-			buf[head] = op[i];
-			head = (head==15) ? 0 : head + 1;
-		}
-		SemaphoreSignal(mutex);
+		SemaphoreSignal(spotsSync);
 	}
 
 	delay(6);
-
+	
 	__critical{
-		SemaphoreWait(mutex);
+		SemaphoreWait(spotsSync);
 		if(spots[0]=='2') {
 			spots[0] = 'X';
 		} else {
 			spots[1] = 'X';
 		}
-		op[3] = '2';
-		op[5] = 'O';
-		op[6] = 'U';
-		op[7] = 'T';
-		op[10] = spots[0];
-		op[12] = spots[1];
-		for(i=0; i<15; i++) {
-			buf[head] = op[i];
-			head = (head==15) ? 0 : head + 1;
-		}
-		SemaphoreSignal(mutex);
+		SemaphoreSignal(spotsSync);
 	}
 
 	SemaphoreSignal(sem);
@@ -115,47 +78,27 @@ void Car2(void){
 
 void Car3(void){
 	SemaphoreWait(sem);
-
+	
 	__critical{
-		SemaphoreWait(mutex);
+		SemaphoreWait(spotsSync);
 		if(spots[0]=='X') {
 			spots[0] = '3';
 		} else {
 			spots[1] = '3';
 		}
-		op[3] = '3';
-		op[5] = ' ';
-		op[6] = 'I';
-		op[7] = 'N';
-		op[10] = spots[0];
-		op[12] = spots[1];
-		for(i=0; i<15; i++) {
-			buf[head] = op[i];
-			head = (head==15) ? 0 : head + 1;
-		}
-		SemaphoreSignal(mutex);
+		SemaphoreSignal(spotsSync);
 	}
 
 	delay(5);
-
+	
 	__critical{
-		SemaphoreWait(mutex);
+		SemaphoreWait(spotsSync);
 		if(spots[0]=='3') {
 			spots[0] = 'X';
 		} else {
 			spots[1] = 'X';
 		}
-		op[3] = '3';
-		op[5] = 'O';
-		op[6] = 'U';
-		op[7] = 'T';
-		op[10] = spots[0];
-		op[12] = spots[1];
-		for(i=0; i<15; i++) {
-			buf[head] = op[i];
-			head = (head==15) ? 0 : head + 1;
-		}
-		SemaphoreSignal(mutex);
+		SemaphoreSignal(spotsSync);
 	}
 
 	SemaphoreSignal(sem);
@@ -166,47 +109,27 @@ void Car3(void){
 
 void Car4(void){
 	SemaphoreWait(sem);
-
+	
 	__critical{
-		SemaphoreWait(mutex);
+		SemaphoreWait(spotsSync);
 		if(spots[0]=='X') {
 			spots[0] = '4';
 		} else {
 			spots[1] = '4';
 		}
-		op[3] = '4';
-		op[5] = ' ';
-		op[6] = 'I';
-		op[7] = 'N';
-		op[10] = spots[0];
-		op[12] = spots[1];
-		for(i=0; i<15; i++) {
-			buf[head] = op[i];
-			head = (head==15) ? 0 : head + 1;
-		}
-		SemaphoreSignal(mutex);
+		SemaphoreSignal(spotsSync);
 	}
 
-	delay(4);
-
+	delay(7);
+	
 	__critical{
-		SemaphoreWait(mutex);
+		SemaphoreWait(spotsSync);
 		if(spots[0]=='4') {
 			spots[0] = 'X';
 		} else {
 			spots[1] = 'X';
 		}
-		op[3] = '4';
-		op[5] = 'O';
-		op[6] = 'U';
-		op[7] = 'T';
-		op[10] = spots[0];
-		op[12] = spots[1];
-		for(i=0; i<15; i++) {
-			buf[head] = op[i];
-			head = (head==15) ? 0 : head + 1;
-		}
-		SemaphoreSignal(mutex);
+		SemaphoreSignal(spotsSync);
 	}
 
 	SemaphoreSignal(sem);
@@ -217,47 +140,27 @@ void Car4(void){
 
 void Car5(void){
 	SemaphoreWait(sem);
-
+	
 	__critical{
-		SemaphoreWait(mutex);
+		SemaphoreWait(spotsSync);
 		if(spots[0]=='X') {
 			spots[0] = '5';
 		} else {
 			spots[1] = '5';
 		}
-		op[3] = '5';
-		op[5] = ' ';
-		op[6] = 'I';
-		op[7] = 'N';
-		op[10] = spots[0];
-		op[12] = spots[1];
-		for(i=0; i<15; i++) {
-			buf[head] = op[i];
-			head = (head==15) ? 0 : head + 1;
-		}
-		SemaphoreSignal(mutex);
+		SemaphoreSignal(spotsSync);
 	}
 
 	delay(3);
-
+	
 	__critical{
-		SemaphoreWait(mutex);
+		SemaphoreWait(spotsSync);
 		if(spots[0]=='5') {
 			spots[0] = 'X';
 		} else {
 			spots[1] = 'X';
 		}
-		op[3] = '5';
-		op[5] = 'O';
-		op[6] = 'U';
-		op[7] = 'T';
-		op[10] = spots[0];
-		op[12] = spots[1];
-		for(i=0; i<15; i++) {
-			buf[head] = op[i];
-			head = (head==) ? 0 : head + 1;
-		}
-		SemaphoreSignal(mutex);
+		SemaphoreSignal(spotsSync);
 	}
 
 	SemaphoreSignal(sem);
@@ -265,7 +168,6 @@ void Car5(void){
 	ThreadExit();
 
 }
-
 
 void Consumer(void) {
 	TMOD |= 0x20;
@@ -288,7 +190,6 @@ void Consumer(void) {
 
 void main(void) {
 	__critical{
-		buf[0] = buf[1] = buf[2] = buf[3] = buf[4] = buf[5] = buf[6] = '\0';
 		head = 0;
 		tail = 0;
 		spots[0] = spots[1] = 'X';
@@ -298,11 +199,17 @@ void main(void) {
 		SemaphoreCreate(sem,2);
 		SemaphoreCreate(spotsSync,1);
 	}
+	
 	ThreadCreate(Car1);
+	do{ delay(2); } while(bitmap==0x0F);
 	ThreadCreate(Car2);
+	do{ delay(2); } while(bitmap==0x0F);
 	ThreadCreate(Car3);
+	do{ delay(2); } while(bitmap==0x0F);
 	ThreadCreate(Car4);
+	do{ delay(2); } while(bitmap==0x0F);
 	Car5();
+	do{ delay(2); } while(bitmap==0x0F);
 	ThreadExit();
 }
 
